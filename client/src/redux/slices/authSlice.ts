1cb50a379cdab29aa06845cdebcf8a3f2f2a05a2
pr4shxnt <student@prashantadhikari7.com.np>
2026-01-17 13:48:29 +0545

    Frontend: nope

--- client/src/redux/slices/authSlice.ts
+++ client/src/redux/slices/authSlice.ts
@@ -1,4 +1,5 @@
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 interface AuthState {
   user: any | null;
@@ -14,37 +15,46 @@ const initialState: AuthState = {
   error: null,
 };
 
+const APIURL = import.meta.env.VITE_BACKEND_URI;
+
+export const loginUser = createAsyncThunk(
+  "login/user",
+  async (userData: any, ThunkAPI) => {
+    try {
+      const res = await axios.post(`${APIURL}/api/auth/login`, userData);
+      return res.data;
+    } catch (error: any) {
+      return ThunkAPI.rejectWithValue(
+        error.response?.data?.message || error.message || "Login failed",
+      );
+    }
+  },
+);
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {
-    loginStart: (state) => {
-      state.loading = true;
-      state.error = null;
-    },
-    loginSuccess: (
-      state,
-      action: PayloadAction<{ user: any; token: string }>
-    ) => {
-      state.loading = false;
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-      localStorage.setItem("token", action.payload.token);
-    },
-    loginFailure: (state, action: PayloadAction<string>) => {
-      state.loading = false;
-      state.error = action.payload;
-    },
-    logout: (state) => {
-      state.user = null;
-      state.token = null;
-      state.loading = false;
-      state.error = null;
-      localStorage.removeItem("token");
-    },
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+        if (action.payload?.token) {
+          state.token = action.payload.token;
+          localStorage.setItem("token", action.payload.token);
+        }
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
+      });
   },
 });
 
-export const { loginStart, loginSuccess, loginFailure, logout } =
-  authSlice.actions;
+export const {} = authSlice.actions;
 export default authSlice.reducer;
