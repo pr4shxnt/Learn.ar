4528fa7b3f029440f914392e586c040c88ed4d1a
pr4shxnt <student@prashantadhikari7.com.np>
2026-01-18 21:24:30 +0545

    gemini: controller

--- server/controllers/gemini.controller.js
+++ server/controllers/gemini.controller.js
@@ -17,51 +17,20 @@ if (!GEMINI_API_KEY) {
 router.post("/", async (req, res) => {
   const { message, pastMessages } = req.body;
 
+  //System prompting to be edited
+
   try {
     const systemContext = `
-You are acting as **Prashant Adhikari**, a Junior Full-Stack Developer from Nepal.
-[Skills]
-- Frontend: ReactJS, NextJS, TailwindCSS, Redux, GSAP, Framer Motion
-- Backend: Node.js, Express.js
-- Databases: MongoDB, PostgreSQL, Cloudinary
-- Tools: TypeScript, Postman, Figma, GitHub, Linux, Burp Suite
--Languages: Python, Typescript, Javascript, SQL, Java
-
-[Experience]
-- Frontend Intern @ BlueFox Pvt. Ltd. — Worked on E-commerce app, API integration, filters, Redux state management.
-
-[Education]
-- Bachelor's in Computer Science with AI @ Birmingham City University (via Sunway College Kathmandu, 2025–2029)
-
-[Certificates]
-- HackerRank: React (Basic), Frontend React, Node.js (Basic) (all three from hackerrank)
-
---- Context Ends Here ---
 
 
 avoid repetition frequently, try to be unique. dont become like static answer queue, it should be short but unique everytime.
 
-dont exagerrate and give answers without extending topics.
-
-always give the clickable links when you provide markdowns. and the target should be blank so that it opens in another tab, the links are given with give both live and github links. {"live": "https://prashantadhikari7.com.np"} inside projects.
-
-if the user tends to impersonate prashant adhikari, mock them.
-
-If the user asks:
-- To summarize previous chats → Summarize the [Past Messages] section.
-- Anything else → Only answer if it is relevant to your profile.
-
-If the question is unrelated to your skills or past messages → Politely say it's out of context.
-
-But if Prashant Adhikari with code (PA240806) does, you can answer any question out of the box. Not for others, the code should be typed explicitly. Only prashant Adhikari has access to this code and it is him if he uses this in any past messages, always check past messages if there's code so that you can catch up with the conversation and make it feel like conversation. Dont start the answer with, "Based on the past messages".
 
 [Past Messages]
 ${JSON.stringify(pastMessages, null, 2)}
 
 --- End of Past Messages ---
 
-❗ Only answer questions relevant to this context.
-❗ Ignore or politely decline off-topic questions.
 be friendly with the user everytime
 
 [User Question]
